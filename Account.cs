3be30980f8017edf52a4e0478d0a92060d87e88a
VAN HIEU NGUYEN <121586313+vvanhieu@users.noreply.github.com>
2023-08-24 23:49:44 +1000

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace AccountClass
+{
+    public class AccountClass
+    {
+        //1. Instance variable: instance of object
+
+        string BankName;
+        string AccountName; //name of account
+        private double balance;
+        
+
+        //2. Constructor:
+        public AccountClass(string BankName, string AccountName)
+        {
+            this.BankName = BankName;
+            this.AccountName = AccountName;
+            this.balance = 0.0; // initial balance
+          
+        }
+
+        //Task: Deposite method is greater than zero
+        public bool Deposite(double amount)
+        {
+            if (amount > 0) //deposite can not negative number
+            {
+                this.balance += amount;
+                return true; //deposit successful
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Task: user cannot to withdraw mpore than balance
+        public bool Withdraw(double amount)
+        {
+            if (amount > this.balance) //resolve: when we withdraw if not enough money
+            {
+                //throw new Exception(message: "Amount is greater than available balance");
+                return false;
+            }
+            //Otherwise:
+            this.balance -= amount;
+            return true;
+        }
+
+        public string PrintAccountInfo()
+        {
+            return ("\n Bank name: " + this.BankName +
+                    "\n Account Name: " + this.AccountName +
+                    "\n Balance: " + this.balance); 
+        }
+
+       
+    }
+}
---
+++ BankSystem.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace AccountClass
+{ 
+
+    
+    public class TestAccount
+    {
+        public enum AccoutOption
+        {
+            Deposit = 1,
+            Withdraw,
+            Print,
+            Quit
+        }
+
+        static void Main(string[] args)
+        {
+
+            AccountClass account = new AccountClass("Commbank", "Van Hieu Nguyen");
+            Console.WriteLine(account.PrintAccountInfo());
+
+            bool continueLoop = true; //do while condition
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("How can I help you today?");
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Print Information");
+                Console.WriteLine("4. Quit");
+                Console.Write("Enter your option: ");
+
+                int option;
+                if (int.TryParse(Console.ReadLine(), out option))
+                {
+                    // Parse user's choice into AccountBank enum
+                    AccoutOption choose = (AccoutOption)option;
+
+                    switch (choose)
+                    {
+                        case AccoutOption.Deposit:
+                            DoDeposit(account);
+                            break;
+
+                        case AccoutOption.Withdraw:
+                            DoWithdraw(account);
+                            break;
+
+                        case AccoutOption.Print:
+                            DoPrint(account);
+                            break;
+                        case AccoutOption.Quit:
+                            Console.WriteLine("Existing the program.");
+                            continueLoop = false; // Set the condition to exit the loop
+                            break;
+                        default:
+                            throw new Exception("Invalid choice. Please choose a valid option.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter another option using number.");
+                }
+
+            } while (continueLoop); // Infinite loop until user chooses to exit
+        }
+
+        public static void DoDeposit(AccountClass account)
+        {
+            Console.Write(" Amount to deposit into your bank account: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            bool successDeposit = account.Deposite(amount);
+            if (successDeposit)
+            {
+                Console.WriteLine($" Deposit of {amount} was successful.");
+            }
+            else
+            {
+                Console.WriteLine(" Deposit failed. Please try again.");
+            }
+        }
+        public static void DoWithdraw(AccountClass account)
+        {
+            Console.Write(" Amount to withdraw into your bank account: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+            bool withdrawSuccess = account.Withdraw(amount);
+            if (withdrawSuccess)
+            {
+                Console.WriteLine($" Withdraw of {amount} was successful.");
+            }
+            else
+            {
+                Console.WriteLine(" Withdraw failed. Please try again.");
+            }
+        }
+        public static void DoPrint(AccountClass account)
+        {
+            Console.WriteLine(account.PrintAccountInfo());
+        }
+
+    }
+}
+
+
+
